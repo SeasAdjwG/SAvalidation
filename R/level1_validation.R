fce4b4a073cafbd110f87f9897f5cb6ebaa892d1
duncan-elliott <duncan_elliott@yahoo.co.uk>
2024-08-13 11:17:33 +0100

    update documentation and small change to overadjustment function

--- R/level1_validation.R
+++ R/level1_validation.R
@@ -1,10 +1,30 @@
-### Extract test for seasonality on linearised series
+#' Extract combined test for seasonality on linearised series
+#'
+#' @param sa_mod An SA object from RJDemetra
+#'
+#' @return A character vector stating the final result of the combined test for seasonality
+#' @export
+#'
+#' @examples
+#' nsa <- data_to_check$nsa
+#' jd_mod <- RJDemetra::x13(nsa)
+#' get_combined_seasonality_test(jd_mod)
 get_combined_seasonality_test <- function(sa_mod){
   result <- sa_mod$diagnostics$combined_test$combined_seasonality_test
   return(result)
 }
 
-### Extract test for calendar effects
+#' Extract test for calendar effects
+#'
+#' @param sa_mod An SA object from RJDemetra
+#'
+#' @return A logical indicating whether calendar regressors found in the SA model object
+#' @export
+#'
+#' @examples
+#' nsa <- data_to_check$nsa
+#' jd_mod <- RJDemetra::x13(nsa)
+#' check_for_calendar_vars(jd_mod)
 check_for_calendar_vars <- function(sa_mod){
   regressors <- sa_mod$regarima$regression.coefficients |>
     rownames() |>
@@ -15,27 +35,54 @@ check_for_calendar_vars <- function(sa_mod){
   return(has_calendar)
 }
 
-### Check if two series are identical
+#' Check if two series are identical
+#'
+#' @param nsa A ts object
+#' @param sa  A ts object
+#'
+#' @return Logical TRUE if all(nsa==sa)
+#' @export
+#'
+#' @examples
+#' check_identical(data_to_check$nsa,data_to_check$sa)
 check_identical <- function(nsa,sa){
   check <- all(nsa==sa)
   return(check)
 }
 
 
-### Check for negatives
+
+#' Check for negatives
+#'
+#' @param series_to_check A numeric vector (or matrix) to check for any negative values
+#'
+#' @return Logical TRUE if any negative values exist
+#' @export
+#'
+#' @examples
+#' check_negatives(data_to_check$nsa)
 check_negatives <- function(series_to_check){
   result <- any(series_to_check < 0)
   return(result)
 }
 
-### Check for over adjustment
 
-check_over_adjustment_qs <- function(sa_mod){
-  result <- sa_mod$user_defined$`diagnostics.seas-lin-qs`[2] <= 0.05
-  return(result)
-}
-
-check_over_adjustment <- function(sa_mod){
+#' Check for over adjustment
+#'
+#' @param sa_mod An SA object from RJDemetra generated with userdefined="preprocessing.model.y_lin"
+#' @param pval numeric value used to define a p-value to use as a threshold for accepting/rejecting null hypothesis (default 0.05)
+#'
+#' @return Logical evidence of
+#' @export
+#'
+#'@description
+#'Tests null hypothesis that acf at lag 4 is greater or equal to zero
+#'
+#' @examples
+#' sa <- data_to_check$sa
+#' jd_mod <- RJDemetra::x13(sa,userdefined="preprocessing.model.y_lin")
+#' check_over_adjustment(jd_mod)
+check_over_adjustment <- function(sa_mod,pval=0.05){
   lin_sa <- sa_mod$user_defined$`preprocessing.model.y_lin` |>
     diff()
   n <- length(lin_sa)
@@ -45,12 +92,23 @@ check_over_adjustment <- function(sa_mod){
   acf_lag_4 <- acf_to_lag_p$acf[p+1,1,1]
 
 
-  result <- acf_lag_4 < qnorm(0.025)/sqrt(n)
+  result <- acf_lag_4 < qnorm(pval)/sqrt(n)
   return(result)
 }
 
-### Level 1 validation check
-
+#' Level 1 validation check
+#'
+#' @param nsa A ts object
+#' @param sa  A ts object
+#' @param default_type Character must be either "X13" (default) or "TS" determining whether X13 or TRAMO-SEATS is used for testing
+#' @param default_spec_nsa Character defining the JDemetra+ specification for tests on the nsa series (default="RSA1")
+#' @param default_spec_sa Character defining the JDemetra+ specification for tests on the sa series (default="RSA2c")
+#'
+#' @return Message about the level 1 validation (pass, pass with warnings or fail)
+#' @export
+#'
+#' @examples
+#' level1_validation(data_to_check$nsa,data_to_check$sa)
 level1_validation <- function(nsa,sa, default_type = "X13", default_spec_nsa="RSA1",default_spec_sa="RSA2c"){
 
   if(!default_type%in%c("X13","TS")){stop("default_type must be `X13` or `TS`")}
